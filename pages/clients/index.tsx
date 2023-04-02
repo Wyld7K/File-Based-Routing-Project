90bf2048311cd61d1ccf7191e0d1d4238e2e7e3d
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-04-02 12:53:30 -0400

    Nav to Dynamic Routes

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 const ClientsPage: React.FC = () => {
 	const router = useRouter();
+
+	const clients = [
+		{ id: 'max', name: 'maxNAME' },
+		{ id: 'man', name: 'manNAME' },
+	];
+
 	return (
 		<div>
 			<h1>Client Page</h1>
+			<ul>
+				{clients.map(client => (
+					<li key={client.id}>
+						<Link href={`/clients/${client.name}`}>{client.name}</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
