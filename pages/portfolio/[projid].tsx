2b7ef961080423ab4759834f9cb91a1dc5770166
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-04-02 12:07:43 -0400

    Add Dynamic Routing

---
+++ pages/portfolio/[projid].tsx
@@ -0,0 +1,11 @@
+const PortfolioProjectPage: React.FC = () => {
+	return (
+		<div>
+			<h1>Portfolio Project Page</h1>
+		</div>
+	);
+};
+
+// Priority routing to concrete pages. For instance list page in portfolio
+
+export default PortfolioProjectPage;
