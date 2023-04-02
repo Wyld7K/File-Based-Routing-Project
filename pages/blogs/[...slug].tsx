0c995f553359794ae135d0da1359ddbb49bab30f
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-04-02 12:44:31 -0400

    Adding Links

---
+++ pages/blogs/[...slug].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+const BlogPostsPage: React.FC = () => {
+	const router = useRouter();
+	return (
+		<div>
+			<h1>DEFAULT</h1>
+		</div>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import Link from 'next/link';
 export default function Home() {
 	return (
 		<>
+			<ul>
+				<li>
+					<Link href="/portfolio">Portfolio</Link>
+				</li>
+				<li>
+					<Link href="/clients">Portfolio</Link>
+				</li>
+			</ul>
 			<h1>Home</h1>
 		</>
 	);
