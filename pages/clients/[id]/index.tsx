b2da05f2e7164083dc288a66c3464f46fbfd4285
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-04-02 13:02:43 -0400

    Nav Programmatically

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,7 +1,19 @@
+import { useRouter } from 'next/router';
 const ClientProjects: React.FC = () => {
+	const router = useRouter();
+	const loadProjectHandler = (): void => {
+		// load data..
+
+		// essentaily a programatic link component
+		router.push({
+			pathname: '/clients/[id]/[projectid]',
+			query: { id: 'max', projectid: 'projectA' },
+		});
+	};
 	return (
 		<div>
 			<h1>Projects of Client</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
 		</div>
 	);
 };
